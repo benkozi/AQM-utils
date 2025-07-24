09e85ee46fb9af9f17fb54ba1a371e47d387d8ff
Ben Koziol <bkoziol@redlineperf.com>
2025-07-24 16:09:42 -0500

    run gc6 test

--- python_utils/aqm-s3-sync/spike/run-gaeac6_verify.sh
+++ python_utils/aqm-s3-sync/spike/run-gaeac6_verify.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+root=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/AQM-utils/python_utils/aqm-s3-sync
+conda_bin=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/miniconda3/condabin/conda
+test_path=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/AQM-utils/python_utils/aqm-s3-sync/spike/gaeac6_verify.py
+
+export PYTHONPATH=${root}/src:${PYTHONPATH}
+
+${conda_bin} run -n benkozi-work --no-capture-output pytest ${test_path}
