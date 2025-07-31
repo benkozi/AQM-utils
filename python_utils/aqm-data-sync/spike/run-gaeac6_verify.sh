e6dee904ed73e80035c8856481b25d87c1ee81a5
Ben Koziol <bkoziol@redlineperf.com>
2025-07-31 17:11:58 -0500

    working aqm workflow

--- python_utils/aqm-data-sync/spike/run-gaeac6_verify.sh
+++ python_utils/aqm-data-sync/spike/run-gaeac6_verify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-root=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/AQM-utils/python_utils/aqm-s3-sync
+root=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/AQM-utils/python_utils/aqm-data-sync
 conda_bin=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/miniconda3/condabin/conda
-test_path=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/AQM-utils/python_utils/aqm-s3-sync/spike/gaeac6_verify.py
+test_path=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/AQM-utils/python_utils/aqm-data-sync/spike/gaeac6_verify.py
 
 export PYTHONPATH=${root}/src:${PYTHONPATH}
 
